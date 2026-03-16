ec092b343a8f4a790ad86e349e1616af5bfeffca
Ira123gr <figor7650@gmail.com>
2026-03-16 20:17:55 -0300

    mais um envio de atividades random 2

---
+++ random ativd 2.php
@@ -0,0 +1,9 @@
+start
+ask for a number 
+if 
+number > 0 and its is integer
+count all the numbers to the chosen number starting by 1 
+then divid all the numbers by 2 and display the ones how the result are integer number
+if not
+display "fuck you"
+end
