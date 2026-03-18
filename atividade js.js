57a963a50a1cf80816a793963748d2857f50c26d
Ira123gr <figor7650@gmail.com>
2026-03-17 21:35:57 -0300

    primeira atividade em java script

---
+++ atividade js.js
@@ -0,0 +1,28 @@
+let idade = 17;
+var nome = "Igor";
+let num1 = 10;
+let num2 = 8;
+let média = num1 + num2;
+let médiafinal = média/2;
+
+if (médiafinal >= 6){
+      mensagem = " Parabêns"
+     } else {
+        mensagem = " Brutal its over pro beta"
+     }
+
+console.log ("o " + nome + " tem " + idade + " anos e sua média final é " + médiafinal + mensagem )
+
+ temp = 18
+ console.log (temp)
+
+ if (média >= -2){
+    temp = 22
+ }
+ console.log(temp )
+
+let fala "Hello,world!"
+
+console.log (fala)
+
+const PI = 3.14
